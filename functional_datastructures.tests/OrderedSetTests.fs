23f24abed1c4ef3818876a59620b3e70352abc1c
isaiah-perumalla <isaiahperumalla@gmail.com>
2012-10-21 21:35:54 +0100

    implementing reb black tree

---
+++ functional_datastructures.tests/OrderedSetTests.fs
@@ -0,0 +1,30 @@
+﻿namespace OrderedSet.Tests
+
+open NUnit.Framework
+open FsUnit
+open OrderedSet
+
+[<TestFixture>]
+type ``Given a set should test for member ship`` ()=
+       
+       let add_to_set set v = TreeSet.add v set
+       [<Test>] member test.
+         ``simple test for equality.`` ()=
+           TreeSet.mem 1 TreeSet.empty  |> should be False
+           
+       [<Test>] 
+       member test.
+        ``test membership on non empty set`` ()= 
+           TreeSet.mem 2 (TreeSet.add 2 TreeSet.empty)  |> should be True
+
+       [<Test>] 
+       member test.
+        ``test balance `` ()= 
+            let set_with_1000 = Seq.fold add_to_set TreeSet.empty {1 .. 1000}
+            TreeSet.mem 100 set_with_1000 |> should be True
+            TreeSet.assert_balanced set_with_1000 |> should be True
+                
+
+
+            
+
---
+++ functional_datastrutures/OrderedSet.fs
@@ -0,0 +1,48 @@
+﻿// Learn more about F# at http://fsharp.net
+
+namespace OrderedSet
+    module TreeSet=
+
+        type 'a rbTree = |E  
+                         | R of 'a rbTree * 'a * 'a rbTree 
+                         | B of 'a rbTree * 'a * 'a rbTree
+
+        let empty = E
+
+        let rec mem x = function
+                | E -> false
+                | R(l,v,r) | B(l,v,r) ->
+                    match compare x v with
+                    |c when c<0 -> mem x l
+                    |c when c>0 -> mem x r
+                    |_ -> true 
+
+        
+        let rec add x = function
+                |E -> B(E,x,E)
+                |R(l,v,r) | B(l,v,r) as n ->
+                    match compare x v with
+                    |c when c<0 -> balance B(add x l, v, r)
+                    |c when c>0 -> balance B(l, v, add x r)
+                    |_ -> n
+
+        let balance = function
+            |E -> 
+
+        let assert_balanced r = 
+            let rec check_black_height root h = 
+                    match root with
+                    |E -> h = 0 
+                    |B(l, _, r) -> check_black_height l (h-1) && check_black_height r (h-1)
+                    |R(l, _, r) -> (check_black_height l h) && (check_black_height r h) 
+            let rec black_height root h =
+                    match root with
+                    | E -> h
+                    | B(l, _, _) -> black_height l (h+1)
+                    | R(l, _, _) -> black_height l h
+            in
+            check_black_height  r (black_height r 0)
+            
+                    
+
+
